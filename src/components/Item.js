17348e3ee7035a9e5f5dd9adf7ca19802738b04a
David Aikman <57766110+daikman@users.noreply.github.com>
2022-04-26 10:51:32 +0100

    add phsopendata and change link rendering

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,21 +5,30 @@ import MobileWarn from './MobileWarn'
 export default function Item({ data }) {
     let i = <>
             <img src={require("../images/"+data.img)} />
-            
             <h2>{data.name}</h2>
             <p>{data.desc}</p>
-            <div className="links">
-                <p>
-                  <a href={data.demo} target="_blank">
-                    live demo 🌐
-                  </a>
-                  &nbsp;|&nbsp;
-                  <a href={data.code} target="_blank">
-                    code ⌨️
-                  </a>
-                </p>
-            </div>   
+            {createLinks(data.links)}
             <MobileWarn warn={data?.mobileWarn}/>
         </>
     return i
+}
+
+function createLinks(links) {
+  const names = Object.keys(links)
+  const hrefs = Object.values(links)
+  
+  const linkComp = names.map((d, i) => {
+    const a = <a href={hrefs[i]} target="_blank">
+      {d}
+    </a>
+    
+    // if the next link exists, add some spacing between them
+    if (!names[i + 1]) return a
+    return <>{a}&nbsp;|&nbsp;</>
+
+  })
+
+  return <div className="links">
+    <p>{linkComp}</p>
+  </div>
 }
