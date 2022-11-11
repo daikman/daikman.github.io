28fdf1bf62951cd8773fa8820301db18a4568106
David Aikman <57766110+daikman@users.noreply.github.com>
2022-11-11 08:00:41 +0000

    remove footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,9 @@ import React from 'react'
 
 export default function Footer() {
     return <div className='footer'>
-        <p>This website was built with&nbsp;
-           <a href="https://reactjs.org/" target="_blank">React.js</a>&nbsp;
-           and the <a href="http://getskeleton.com/" target="_blank">skeleton CSS library</a>.&nbsp;
-           See the source code <a href='https://github.com/daikman/daikman.github.io' target="_blank">here</a>.
-        </p>
+        {/* <p>
+          <a href="mailto:davidjaikman@outlook.com">Email me</a>
+          <a>© David Aikman 2022</a>
+        </p> */}
   </div>
 }
